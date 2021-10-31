abf8c36d4655677e11312e70d5aeeab89d9951b3
Sahan Perera <sahanyuganthamail@gmail.com>
2021-10-31 11:38:52 +0530

    updated database configs to remote

--- src/main/java/com/cohort4/cohort4jaxrsapp/config/DbConnection.java
+++ src/main/java/com/cohort4/cohort4jaxrsapp/config/DbConnection.java
@@ -13,9 +13,15 @@ public class DbConnection {
 	
 	private static Logger logger = LogManager.getLogger(DbConnection.class);
 	
-	private static final String DB_URL = "jdbc:mysql://localhost:3306/cohort4_db";
-	private static final String DB_USER = "root";
-	private static final String DB_PASSWORD = "root";
+//	private static final String DB_URL = "jdbc:mysql://localhost:3306/cohort4_db";
+//	private static final String DB_USER = "root";
+//	private static final String DB_PASSWORD = "root";
+	
+	//mysql://ba30ae0e1ef3c4:ae58e4a6@us-cdbr-east-04.cleardb.com/heroku_65f96ba3e97ae85?reconnect=true
+	
+	private static final String DB_URL = "jdbc:mysql://us-cdbr-east-04.cleardb.com/heroku_65f96ba3e97ae85";
+	private static final String DB_USER = "ba30ae0e1ef3c4";
+	private static final String DB_PASSWORD = "ae58e4a6";
 	
 
 	private static DbConnection newInstance = null;
